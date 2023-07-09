8e233d26f1f391bcac528a924ddca92a4e107a64
Marcelo Amaral <marceloamaral386@gmail.com>
2023-07-09 01:52:21 -0300

    Refactoring and resetting cooldown

--- scenes/attacks/SwordsmanAttack.gd
+++ scenes/attacks/SwordsmanAttack.gd
@@ -3,8 +3,6 @@ extends Area2D
 var by_player: bool
 var class_: int
 
-var entities_hit = []
-
 func init(direction: Vector2, by_player_: bool, is_zombie_: bool = false):
 	self.rotation = direction.angle()
 	self.by_player = by_player_
@@ -16,14 +14,13 @@ func _ready():
 		set_collision_mask_bit(1, true)
 		set_collision_mask_bit(2, false)
 
-func _physics_process(_delta):
-	for body in self.get_overlapping_bodies():
-		if body.has_method("take_damage") and not body in entities_hit:
-			entities_hit.append(body)
-			if by_player:
-				body.take_damage(Classes.damage[class_] * 3, class_)
-			else:
-				body.take_damage(Classes.damage[class_], class_)
+func _on_AttackArea_body_entered(body):
+	if body is Entity:
+		if by_player:
+			body.take_damage(Classes.damage[class_] * 3, class_)
+		else:
+			body.take_damage(Classes.damage[class_], class_)
 
 func _on_AttackTimer_timeout():
 	queue_free()
+
